cce0a3389ec444f6c23b8063ff8c1b5066046c69
NurlanGuliyev <101888525+NurlanGuliyev@users.nnoreply.github.com>
2024-07-25 14:28:11 +0300

    structure update

--- src/App.js
+++ src/App.js
@@ -1,73 +1,63 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
+import { routes, storageKeys, strings } from './constants';
 
-function App() {
+const App = () => {
     const [message, setMessage] = useState('');
+
     const [responses, setResponses] = useState([]);
+
     const [clientId, setClientId] = useState(null);
 
-    useEffect(() => {
-        // Generate and store clientId if not already stored
-        let storedClientId = localStorage.getItem('clientId');
-        if (!storedClientId) {
-            storedClientId = generateUUID();
-            localStorage.setItem('clientId', storedClientId);
-        }
-        setClientId(storedClientId);
-    }, []);
-
-    const generateUUID = () => {
-        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-            const r = (Math.random() * 16) | 0, v = c === 'x' ? r : (r & 0x3) | 0x8;
-            return v.toString(16);
-        });
-    };
+    const generateUUID = () => Math.ceil(Math.random() * 999999999).toString();
 
     const sendMessage = async () => {
-        if (!message.trim() || !clientId) return;
+        if (!message || !clientId) return;
 
         try {
-            const res = await axios.post('https://chatbot-backend-mr8u.onrender.com/api/chat', { message, clientId });
-            handleResponse(res.data);
-            setMessage(''); // Clear input field after sending message
+            const { data } = await axios.post(routes.sendMessage, { message, clientId });
+
+            setResponses(data.messages);
+
+            setMessage('');
         } catch (error) {
-            console.error('Error sending message:', error);
+            console.error(strings.errorSendMessage, error);
         }
     };
 
-    const handleResponse = (data) => {
-        // Filter out duplicate messages before updating state
-        const newResponses = data.messages.filter((msg) => !responses.some((r) => r.content === msg.content));
-        setResponses([...responses, ...newResponses]);
+    const handleSession = () => {
+        const storedClientId = localStorage.getItem(storageKeys.clientId) ||  generateUUID();
 
-        // Check if there's fetched data and add it to the responses
-        if (data.fetchedData) {
-            const fetchedDataMessage = {
-                role: 'assistant',
-                content: data.fetchedData.map(item => `Estate: ${item.title} - <a href="${item.link}" target="_blank">${item.link}</a>`).join('<br />')
-            };
-            setResponses(prevResponses => [...prevResponses, fetchedDataMessage]);
-        }
-    };
+        localStorage.setItem(storageKeys.clientId, storedClientId);
+
+        setClientId(storedClientId);
+    }
+
+    useEffect(() => {
+        handleSession();
+    }, []);
+
 
     return (
-        <div className="App">
+        <div className="app">
             <h1 className="app-title">ChatBot Program</h1>
             <div className="chat-box">
                 {responses.map((res, index) => (
-                    <div key={index} className={`message ${res.role}`} dangerouslySetInnerHTML={{ __html: `<strong>${res.role}:</strong> ${res.content}` }} />
+                    <div key={index} className={`message ${res.role}`}  >
+                        <strong>{res.role}:</strong> {res.content}
+                    </div>
                 ))}
             </div>
             <div className="input-box">
                 <input
                     type="text"
                     value={message}
-                    onChange={(e) => setMessage(e.target.value)}
+                    onChange={(e) => setMessage(e.target.value.trim())}
                     onKeyPress={(e) => e.key === 'Enter' && sendMessage()}
-                    placeholder="Type your message..."
+                    placeholder={strings.inputPlaceholder}
                 />
-                <button onClick={sendMessage}>Send</button>
+                <button onClick={sendMessage}>{strings.send}</button>
             </div>
         </div>
     );
---
+++ src/constants.js
@@ -0,0 +1,13 @@
+export const routes = {
+    sendMessage: 'https://chatbot-backend-mr8u.onrender.com/api/chat'
+}
+
+export const storageKeys = {
+    clientId: 'clientId'
+}
+
+export const strings = {
+    errorSendMessage: 'Error sending message:',
+    inputPlaceholder: "Type your message...",
+    send : 'Send'
+}
